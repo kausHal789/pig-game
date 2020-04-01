489a224a76fc762239a1434f85f01e13b5ca9e69
Kaushal <50080954+kausHal789@users.noreply.github.com>
2020-04-01 07:20:34 +0530

    .

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 var activePlayer, scores, roundScore, gameState;
 
-//document.querySelector('.dice').style.display = 'none';
-
-/*
-document.querySelector('.dice').addEventListener('click', function() {
-  
-});
-
-document.querySelector('.dice').addEventListener('click', function-name);
-
-*/
-
-
 function rollDice() {
   if (gameState) {
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -23,9 +11,6 @@ function rollDice() {
       nextPlayer();
       return;
     }
-  //  dice += parseInt(document.querySelector("#current-" + activePlayer).textContent);
-  //  document.querySelector("#current-" + activePlayer).textContent = dice;
-  //  another method
     roundScore += dice;
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   }
@@ -38,7 +23,7 @@ function hold() {
     document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
 
-    if(scores[activePlayer] >= 10) { 
+    if(scores[activePlayer] >= 20) { 
       document.getElementById('name-' + activePlayer).textContent = 'Winner';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -53,8 +38,6 @@ function hold() {
 }
 
 function nextPlayer() {
-//    scores[activePlayer] += parseInt(document.querySelector("#current-" + activePlayer).textContent);
-//    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
   roundScore = 0;
   activePlayer = (activePlayer === 0) ? 1 : 0;
   document.querySelector('#current-0').textContent = 0;
